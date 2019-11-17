d4385bcfb469546554014402fb39611a6ad5f2b9
Folkheart <leandroadrianpaz@gmail.com>
2019-11-17 20:42:40 -0300

    config

--- config.def.h
+++ config.def.h
@@ -13,9 +13,10 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_pink[]        = "#f000ff";
+static const char col_black[]       = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, col_balck },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_pink  },
 };
 
